c8b98e1332d65ee119dc0bfd117464e2b4b0bed4
unknown <Robert.Aron.tx@gmail.com>
2019-02-08 19:32:18 -0600

    tutorial helper

---
+++ Assets/GameScripts/TutorialSpecific/TutorialGlowHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class TutorialGlowHelper : MonoBehaviour {
+	[SerializeField] float playAt;
+	[SerializeField] float loopTime;
+	float countedTime;
+	ParticleSystem ps;
+	// Use this for initialization
+	private void Awake() {
+		countedTime = loopTime - playAt;
+		ps = GetComponent<ParticleSystem>();
+	}
+	private void FixedUpdate() {
+		countedTime+=Time.fixedDeltaTime;
+		if(countedTime>=loopTime){
+			Debug.Log(countedTime>=loopTime);
+			countedTime-=loopTime;
+			ps.Stop();
+			ps.Clear();
+			ps.Play();
+		}
+	}
+}
