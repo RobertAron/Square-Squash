2db060b992c19477b36604d01f549ab12641e483
unknown <Robert.Aron.tx@gmail.com>
2019-02-23 23:26:30 -0600

    it's all coming together...

---
+++ Assets/GameScripts/UI/MainMenu/MainMenuInfoSetter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MainMenuInfoSetter : MonoBehaviour {
+	[SerializeField] Text userName;
+	[SerializeField] Text levelText;
+	[SerializeField] Text topScoreText;
+	[SerializeField] Text expText;
+
+	// Use this for initialization
+	void Start () {
+		string playerName = PlayerPrefs.GetString(PrefKeys.playerName);
+		int playerLevel = PlayerPrefs.GetInt(PrefKeys.playerLevel);
+		int bestScore = PlayerPrefs.GetInt(PrefKeys.bestScore);
+		int playerExp = PlayerPrefs.GetInt(PrefKeys.playerExp);
+		userName.text = playerName;
+		levelText.text = "Level: " + playerLevel.ToString();
+		topScoreText.text = "Best: " + bestScore.ToString();
+		expText.text = "EXP: " +playerExp+"/" +StaticCalcs.experienceToLevel(playerLevel);
+	}
+}
---
+++ Assets/UpdateUsernameUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateUsernameUI : MonoBehaviour {
+	[SerializeField] Text newUsername;
+	[SerializeField] UpdatePlayerData upd;
+	// Use this for initialization
+	public void UpdateUsername(){
+		Debug.Log("called to update user name with...");
+		Debug.Log(newUsername.text);
+		PlayerPrefs.SetString(PrefKeys.playerName,newUsername.text);
+		upd.SetPlayerName(newUsername.text);
+	}
+}
